0559d8ae075ebe665dfbee4a3cad95f88eb478f2
Anuvarshini KS <anuvarshini.ad0408@gmail.com>
2026-02-10 17:40:23 +0530

    Day12 task

---
+++ src/com/kovanlabs/intern/GenericsAndWildcards/WildcardSum.java
@@ -0,0 +1,24 @@
+package com.kovanlabs.intern.GenericsAndWildcards;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WildcardSum {
+    public static double addList(List<? extends Number> list)
+    {
+        double sum = 0.0;
+        for(Number num : list)
+        {
+            sum += num.doubleValue();
+        }
+        return sum;
+    }
+    public static void main(String[]args)
+    {
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Double> doubleList = Arrays.asList(2.3,8.5,6.7);
+        System.out.println("The sum of Integer list is : "+addList(intList));
+        System.out.println("The sum of Double list is : "+addList(doubleList));
+     }
+}
